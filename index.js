89588fc386655d097846af5790d9279d27b6315f
lixing <18717725112@163.com>
2019-04-07 19:42:41 +0800

    finish

--- index.js
+++ index.js
@@ -23,13 +23,16 @@ const roleUserGroup = new RoleUserGroup();
  * user module 
  * */
 const userInsert = async(ctx) => {
-    ctx.response.body = await user.insert_user(ctx.query['userName']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await user.insert_user(ctx.query['userName']) };
 }
 const userSearch = async(ctx) => {
-    ctx.response.body = await user.search_user();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await user.search_user() };
 }
 const userDelete = async(ctx) => {
-    ctx.response.body = await user.delete_user(ctx.query['userId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await user.delete_user(ctx.query['userId']) };
 }
 app.use(route.post('/user', userInsert));
 app.use(route.get('/user', userSearch));
@@ -39,13 +42,16 @@ app.use(route.delete('/user', userDelete));
  *role module
  */
 const roleInsert = async(ctx) => {
-    ctx.response.body = await role.insert_role(ctx.query['roleName']);
+    ctx.response.type = 'json';
+    ctx.response.body = { messaeg: await role.insert_role(ctx.query['roleName']) };
 }
 const roleSearch = async(ctx) => {
-    ctx.response.body = await role.search_role();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await role.search_role() };
 }
 const roleDelete = async(ctx) => {
-    ctx.response.body = await role.delete_role(ctx.query['roleId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await role.delete_role(ctx.query['roleId']) };
 }
 app.use(route.post('/role', roleInsert));
 app.use(route.get('/role', roleSearch));
@@ -54,13 +60,16 @@ app.use(route.delete('/role', roleDelete));
  *privilege module
  */
 const privilegeInsert = async(ctx) => {
-    ctx.response.body = await privilege.insert_privilege(ctx.query['privilegeDescEn'], ctx.query['privilegeDescCh']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await privilege.insert_privilege(ctx.query['privilegeDescEn'], ctx.query['privilegeDescCh']) };
 }
 const privilegeSearch = async(ctx) => {
-    ctx.response.body = await privilege.search_privilege();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await privilege.search_privilege() };
 }
 const privilegeDelete = async(ctx) => {
-    ctx.response.body = await privilege.delete_privilege(ctx.query['privilegeId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await privilege.delete_privilege(ctx.query['privilegeId']) };
 }
 app.use(route.post('/privilege', privilegeInsert));
 app.use(route.get('/privilege', privilegeSearch));
@@ -71,13 +80,16 @@ app.use(route.delete('/privilege', privilegeDelete));
  *bind role and user
  */
 const userRoleInsert = async(ctx) => {
-    ctx.response.body = await userRole.insert_user_role(ctx.query['userId'], ctx.query['roleId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userRole.insert_user_role(ctx.query['userId'], ctx.query['roleId']) };
 }
 const userRoleSearch = async(ctx) => {
-    ctx.response.body = await userRole.search_user_role();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await userRole.search_user_role() };
 }
 const userRoleDelete = async(ctx) => {
-    ctx.response.body = await userRole.delete_user_role(ctx.query['userId'], ctx.query['roleId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userRole.delete_user_role(ctx.query['userId'], ctx.query['roleId']) };
 }
 app.use(route.post('/userRole', userRoleInsert));
 app.use(route.get('/userRole', userRoleSearch));
@@ -87,13 +99,16 @@ app.use(route.delete('/userRole', userRoleDelete));
  *role-privilege module
  */
 const rolePrivilegeInsert = async(ctx) => {
-    ctx.response.body = await rolePrivilege.insert_role_privilege(ctx.query['roleId'], ctx.query['privilegeId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await rolePrivilege.insert_role_privilege(ctx.query['roleId'], ctx.query['privilegeId']) };
 }
 const rolePrivilegeSearch = async(ctx) => {
-    ctx.response.body = await rolePrivilege.search_role_privilege();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await rolePrivilege.search_role_privilege() };
 }
 const rolePrivilegeDelete = async(ctx) => {
-    ctx.response.body = await rolePrivilege.delete_role_privilege(ctx.query['roleId'], ctx.query['privilegeId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await rolePrivilege.delete_role_privilege(ctx.query['roleId'], ctx.query['privilegeId']) };
 }
 app.use(route.post('/rolePrivilege', rolePrivilegeInsert));
 app.use(route.get('/rolePrivilege', rolePrivilegeSearch));
@@ -103,7 +118,8 @@ app.use(route.delete('/rolePrivilege', rolePrivilegeDelete));
  * 校验权限
  *  */
 const userCkeckPrivilege = async(ctx) => {
-    ctx.response.body = await user.check_user(ctx.query['userId'], ctx.query['privilegeId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await user.check_user(ctx.query['userId'], ctx.query['privilegeId']) };
 }
 app.use(route.get('/user/check', userCkeckPrivilege));
 
@@ -112,13 +128,16 @@ app.use(route.get('/user/check', userCkeckPrivilege));
  * userGroup
  */
 const userGroupInsert = async(ctx) => {
-    ctx.response.body = await userGroup.insert_userGroup(ctx.query['groupName']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userGroup.insert_userGroup(ctx.query['groupName']) };
 }
 const userGroupSearch = async(ctx) => {
-    ctx.response.body = await userGroup.search_userGroup();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await userGroup.search_userGroup() };
 }
 const userGroupDelete = async(ctx) => {
-    ctx.response.body = await userGroup.delete_userGroup(ctx.query['groupId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userGroup.delete_userGroup(ctx.query['groupId']) };
 }
 app.use(route.post('/userGroup', userGroupInsert));
 app.use(route.get('/userGroup', userGroupSearch));
@@ -129,13 +148,16 @@ app.use(route.delete('/userGroup', userGroupDelete));
  * user-userGroup
  */
 const userUserGroupInsert = async(ctx) => {
-    ctx.response.body = await userUserGroup.insert_userUserGroup(ctx.query['userGroupId'], ctx.query['userId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userUserGroup.insert_userUserGroup(ctx.query['userGroupId'], ctx.query['userId']) };
 }
 const userUserGroupSearch = async(ctx) => {
-    ctx.response.body = await userUserGroup.search_userUserGroup();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await userUserGroup.search_userUserGroup() };
 }
 const userUserGroupDelete = async(ctx) => {
-    ctx.response.body = await userUserGroup.delete_userUserGroup(ctx.query['userGroupId'], ctx.query['userId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await userUserGroup.delete_userUserGroup(ctx.query['userGroupId'], ctx.query['userId']) };
 }
 app.use(route.post('/userUserGroup', userUserGroupInsert));
 app.use(route.get('/userUserGroup', userUserGroupSearch));
@@ -146,13 +168,16 @@ app.use(route.delete('/userUserGroup', userUserGroupDelete));
  * role-userGroup
  */
 const roleUserGroupInsert = async(ctx) => {
-    ctx.response.body = await roleUserGroup.insert_roleUserGroup(ctx.query['userGroupId'], ctx.query['roleId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await roleUserGroup.insert_roleUserGroup(ctx.query['userGroupId'], ctx.query['roleId']) };
 }
 const roleUserGroupSearch = async(ctx) => {
-    ctx.response.body = await roleUserGroup.search_roleUserGroup();
+    ctx.response.type = 'json';
+    ctx.response.body = { data: await roleUserGroup.search_roleUserGroup() };
 }
 const roleUserGroupDelete = async(ctx) => {
-    ctx.response.body = await roleUserGroup.delete_roleUserGroup(ctx.query['userGroupId'], ctx.query['roleId']);
+    ctx.response.type = 'json';
+    ctx.response.body = { message: await roleUserGroup.delete_roleUserGroup(ctx.query['userGroupId'], ctx.query['roleId']) };
 }
 app.use(route.post('/roleUserGroup', roleUserGroupInsert));
 app.use(route.get('/roleUserGroup', roleUserGroupSearch));
